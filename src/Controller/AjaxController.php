137c8911a86b22aa5af5b3a02b004fb917fdf3bd
david <karajanchang@gmail.com>
2019-03-04 17:45:36 +0800

    完成 Datatables 分頁

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -8,6 +8,7 @@
 
 namespace Zhyu\Controller;
 
+use Illuminate\Pagination\Paginator;
 use Zhyu\Repositories\Contracts\RepositoryInterface;
 use Zhyu\Repositories\Criterias\CriteriaApp;
 use Zhyu\Repositories\Eloquents\RepositoryApp;
@@ -19,13 +20,35 @@ class AjaxController extends ZhyuController
     {
         $this->middleware(['web', 'auth', 'checklogin']);
     }
+    private function currentPage(){
+        $start = request()->input('start');
+        $start = is_null($start) ? 0 : $start;
+        $start = (int) $start;
+
+        $limit = request()->input('length');
+        $limit = is_null($limit) ? 50 : $limit;
+        $limit = (int) $limit;
+
+        $currentPage = ($start / $limit) + 1;
+        Paginator::currentPageResolver(function () use ($currentPage) {
+            return $currentPage;
+        });
+    }
 
     public function index($model, $key, $limit = 50)
     {
         RepositoryApp::bind($model);
         $repository = app()->make(RepositoryInterface::class);
-        CriteriaApp::bind($repository, $model.'.'.$key);
-        $rows = $repository->all();
-        return $rows;
+        CriteriaApp::ajaxBind($repository, $model.'.'.$key);
+
+        $this->currentPage();
+        $res = $repository->paginate($limit);
+        //$repository->offset($start)->limit($limit);
+        return [
+            'draw' => intval(request()->input('draw')),
+            'recordsTotal' =>  $res->total(),
+            'recordsFiltered' => $res->total(),
+            'data' => $res->items(),
+        ];
     }
 }
