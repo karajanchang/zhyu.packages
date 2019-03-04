93073622cbaa08f466d2d3984d9e59973f1e922b
david <karajanchang@gmail.com>
2019-03-05 06:14:14 +0800

    [Datatables] 修改成新config step1

--- src/Datatables/JsCreate.php
+++ src/Datatables/JsCreate.php
@@ -2,105 +2,112 @@
 
 namespace Zhyu\Datatables;
 
-class JsCreate {
-	private $id;
-    private $ajax;
-	private $js;
-	private $cols_display;
-	private $no_orderable_cols;
-	private $render;
+use Illuminate\Database\Eloquent\Model;
 
-	public function __construct(Render $render)
+class JsCreate {
+    private $id;
+    private $ajax;
+    private $js;
+    private $cols_display;
+    private $no_orderable_cols;
+    private $render;
+    private $model;
+
+    public function __construct(Render $render)
     {
         $this->render = $render;
     }
+    public function model(Model $model){
+        return $this->model = $model;
+    }
 
     public function init($config, $varName = 'table'){
-		if(isset($config['id'])){
-			$this->id = $config['id'];
-		}
-		if(isset($config['ajax'])){
-			$this->ajax = $config['ajax'];
-		}
-		if(isset($config['cols_display'])){
-			$this->cols_display = $config['cols_display'];
-		}
+        if(isset($config['id'])){
+            $this->id = $config['id'];
+        }
+        $this->ajax = route('ajax', [ 'model' => class_basename($this->model->getTable()), 'act' => 'ajax' ]);
+        if(isset($config['ajax'])){
+            $this->ajax = $config['ajax'];
+        }
+        if(isset($config['cols_display'])){
+            $this->cols_display = $config['cols_display'];
+        }
         if (isset($config['no_orderable_cols'])) {
             $this->no_orderable_cols = $config['no_orderable_cols'];
         }
-		$this->head($varName);
-		return $this->js;
-	}
-	private function head($varName){
-		$this->js="var $varName = '';\n";
-		$this->js.="$(function() {\n";
-		$this->js.="\t$varName = $('#".$this->id."').DataTable({\n";
-		$this->js.="\t\t".'"language": {'."\n".
-                   "\t\t\t".    '"url": "/js/datatable.tw.json"'."\n".
-                   "\t\t".  '},'."\n";
+        $this->head($varName);
+        return $this->js;
+    }
+    private function head($varName){
+        $this->js="var $varName = '';\n";
+        $this->js.="$(function() {\n";
+        $this->js.="\t$varName = $('#".$this->id."').DataTable({\n";
+        $this->js.="\t\t".'"language": {'."\n".
+            "\t\t\t".    '"url": "/js/datatable.tw.json"'."\n".
+            "\t\t".  '},'."\n";
         $this->js.="\t\t".'"serverSide": true,'."\n";
-		$this->js.="\t\t".'"stateSave": true,'."\n";
-		$this->js.="\t\t".'"deferRender": true,'."\n";
+        $this->js.="\t\t".'"stateSave": true,'."\n";
+        $this->js.="\t\t".'"deferRender": true,'."\n";
         $this->js.="\t\t".'"stateDuration": 60 * 60,'."\n";
         $this->js.="\t\t".'"iDisplayLength": 50,'."\n";
         $this->js.="\t\t".'"pagingType": "simple_numbers",'."\n";
         //$this->js.="\t\t".'"order": [0, "desc"],'."\n";
-		$this->js.="\t\t".'"ajax": {'."\n";
-		$this->js.="\t\t".'url: "'.$this->ajax.'",'."\n";
+        $this->js.="\t\t".'"ajax": {'."\n";
+        $this->js.="\t\t".'url: "'.$this->ajax.'",'."\n";
         //$this->js.="\t\t".'url: "/new/api/hawk.json",'."\n";
         $this->js.="\t\t".'type:"get",'."\n";
         $this->js.="\t\t".'"dataSrc": "data",'."\n";
         $this->js.="\t\t".'"error": function (xhr, error, thrown) {}'."\n";
-		$this->js.="\t\t".'},'."\n";
-		
-		$this->js.="\t\t".'"columns":['."\n";
-		
-		$this->cols();
-		
-		$this->js.="\t\t"."]\n";
-		$this->js.="\t\t"."});";
-		$this->js.="\t"."});";
-	}
-	private function cols(){
-		if(count($this->cols_display)){
-			$ccs = [];
-			foreach($this->cols_display as $col => $cols){
-				$cc="\t\t".'{'."\n";
-                    $bbbs = [];
-                    $bbbs[] ="\t\t\t". 'data: "'.$col.'"'."\n";
-                    if (isset($this->no_orderable_cols) && in_array($col, $this->no_orderable_cols)) {
-                        $bbbs[] = "\t\t\t". "'orderable': false" . "\n";
+        $this->js.="\t\t".'},'."\n";
+
+        $this->js.="\t\t".'"columns":['."\n";
+
+        $this->cols();
+
+        $this->js.="\t\t"."]\n";
+        $this->js.="\t\t"."});";
+        $this->js.="\t"."});";
+    }
+    private function cols(){
+        if(count($this->cols_display)){
+            $ccs = [];
+            foreach($this->cols_display as $col => $cols){
+                $cc="\t\t".'{'."\n";
+                $bbbs = [];
+                $bbbs[] ="\t\t\t". 'data: "'.$col.'"'."\n";
+                if (isset($this->no_orderable_cols) && in_array($col, $this->no_orderable_cols)) {
+                    $bbbs[] = "\t\t\t". "'orderable': false" . "\n";
+                }
+                if(isset($cols['cols_css'])) {
+                    $css = $cols['cols_css'];
+                    if(is_array($cols['cols_css'])){
+                        $css = join(' ', $cols['cols_css']);
                     }
-                    if(isset($cols['cols_css'])) {
-                        $css = $cols['cols_css'];
-                        if(is_array($cols['cols_css'])){
-                            $css = join(' ', $cols['cols_css']);
+                    $bbbs[] ="\t\t\t". 'className: "' . $css . '"' . "\n";
+                }
+
+                if(isset($cols['render']) && count($cols['render'])){
+                    if(!isset($cols['render']['url']) && !isset($cols['render']['boolean']) ){
+                        $bbbs[] = $this->render->string($cols['render']['content']);
+                    }else {
+                        if (isset($cols['render']['url'])) {
+                            $bbbs[] = $this->render->url($cols['render']);
                         }
-                        $bbbs[] ="\t\t\t". 'className: "' . $css . '"' . "\n";
-                    }
-
-                    if(isset($cols['render']) && count($cols['render'])){
-                        if(!isset($cols['render']['url']) && !isset($cols['render']['boolean']) ){
-                            $bbbs[] = $this->render->string($cols['render']['content']);
-                        }else {
-                            if (isset($cols['render']['url'])) {
-                                $bbbs[] = $this->render->url($cols['render']);
-                            }
-                            if (isset($cols['render']['boolean'])) {
-                                $bbbs[] = $this->render->boolean($cols['render']);
-                            }
+                        if (isset($cols['render']['boolean'])) {
+                            $bbbs[] = $this->render->boolean($cols['render']);
                         }
-                    }else{
-                        $bbbs[] = $this->render->string('${row.'.$col.'}');
                     }
+                }else{
+                    $bbbs[] = $this->render->string('${row.'.$col.'}');
+                }
 
-                    $cc.=join("\t\t".','."\n", $bbbs);
+                $cc.=join("\t\t".','."\n", $bbbs);
 
 
-				$cc.="\t\t".'}'."\n";
-				$ccs[] = $cc;
-			}
-			$this->js.=join(',', $ccs);
-		}
-	}
+                $cc.="\t\t".'}'."\n";
+                $ccs[] = $cc;
+            }
+            $this->js.=join(',', $ccs);
+        }
+    }
 }
--- src/Datatables/TableCreate.php
+++ src/Datatables/TableCreate.php
@@ -2,58 +2,68 @@
 
 namespace Zhyu\Datatables;
 
+use Illuminate\Database\Eloquent\Model;
+
 class TableCreate {
-	private $id;
-	private $css;
-	private $cols_display = [];
-	private $table = '';
-	public function init($config){
-		if(isset($config['id'])){
-			$this->id = $config['id'];
-		}
-		if(isset($config['css'])){
-			$this->css = $config['css'];
-		}
-		if(isset($config['cols_display'])){
-			$this->cols_display = $config['cols_display'];
-		}
-		$this->head();
-		$this->body();
-		
-		return $this->table;
-	}
-	private function head(){
-		$this->table = '<table id="'.$this->id.'" '.$this->css($this->css).'>';
-	}
-	
-	private function body(){
-		$this->table.='<thead>';
-		$this->table.='<tr>';
-		$this->table.=$this->cols();
-		$this->table.='</tr>';
-		$this->table.='</thead>';
-		$this->table.='<tbody>';
-		$this->table.='</tbody>';
-		$this->table.='</table>';
-	}
-	private function cols(){
-		$str='';
-		if(count($this->cols_display)){
-			foreach($this->cols_display as $cols){
-				$css = isset($cols['css']) ? $this->css($cols['css']) : '';
-				$str.='<th '.$css.'>'.$cols['title'].'</th>';
-			}
-		}
-		return $str;
-	}
-	
-	
-	private function css($css){
-		if(is_string($css)){
-			return ' class="'.$css.'"';
-		}
-		if(is_array($css)){
-			return ' class="'.join(' ', $css).'"';
-		}
-	}
+    private $id;
+    private $css;
+    private $cols_display = [];
+    private $table = '';
+    private $model;
+
+    public function model(Model $model){
+        return $this->model = $model;
+    }
+    public function init($config){
+        if(isset($config['id'])){
+            $this->id = $config['id'];
+        }
+        if(isset($config['css'])){
+            $this->css = $config['css'];
+        }
+        if(isset($config['cols_display'])){
+            $this->cols_display = $config['cols_display'];
+        }
+        $this->head();
+        $this->body();
+
+        return $this->table;
+    }
+    private function head(){
+        $this->table = '<table id="'.$this->id.'" '.$this->css($this->css).'>';
+    }
+
+    private function body(){
+        $this->table.='<thead>';
+        $this->table.='<tr>';
+        $this->table.=$this->cols();
+        $this->table.='</tr>';
+        $this->table.='</thead>';
+        $this->table.='<tbody>';
+        $this->table.='</tbody>';
+        $this->table.='</table>';
+    }
+    private function cols(){
+        $tableName = $this->model->getTable();
+        $str='';
+        if(count($this->cols_display)){
+            foreach($this->cols_display as $cols){
+                $css = isset($cols['css']) ? $this->css($cols['css']) : '';
+                $title = $tableName.'.'.$cols['title'];
+                $str.='<th '.$css.'>'.trans($title).'</th>';
+//				$str.='<th '.$css.'>'.$cols['title'].'</th>';
+            }
+        }
+        return $str;
+    }
+
+
+    private function css($css){
+        if(is_string($css)){
+            return ' class="'.$css.'"';
+        }
+        if(is_array($css)){
+            return ' class="'.join(' ', $css).'"';
+        }
+    }
 }
