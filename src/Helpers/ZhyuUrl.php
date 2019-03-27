041bb4def032359eaca8fdd0fac278a5492ea040
david <karajanchang@gmail.com>
2019-03-27 19:08:34 +0800

    加入 ZhyuUrl Facades

---
+++ src/Helpers/ZhyuUrl.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 2019-03-26
+ * Time: 13:26
+ */
+
+namespace Zhyu\Helpers;
+
+
+class ZhyuUrl
+{
+    public function encode()
+    {
+    }
+    public function decode(){
+
+    }
+}
