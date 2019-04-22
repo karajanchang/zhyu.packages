c779619935a4eaf1ef9aa8316cce2a832345217d
david <karajanchang@gmail.com>
2019-04-22 11:57:29 +0800

    [sidemenu] add logout

--- src/blades/blocks/sidemenu.blade.php
+++ src/blades/blocks/sidemenu.blade.php
@@ -64,6 +64,7 @@
                     @endif
                 @endforeach
             </ul>
+            <ul><li><span class="mdi mdi-logout-variant"></span> <a href="{{ route('logout') }}" class="text-justify" style="color: #CCC;">登出</a></li></ul>
         @endif
     </div>
 </div>
