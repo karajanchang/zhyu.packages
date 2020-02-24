0d3a3036378bc76958ab67b65a53b3cf66376ce6
david <karajanchang@gmail.com>
2020-02-25 07:50:17 +0800

    [PlusMinusConvert] 預設值變為null，null時回0

--- src/Tools/PlusMinusConvert.php
+++ src/Tools/PlusMinusConvert.php
@@ -6,7 +6,12 @@ namespace Zhyu\Tools;
 //-----數值負轉正，正轉負
 class PlusMinusConvert
 {
-    public function run(int $number = 0){
+    public function run($number = null){
+        if(is_null($number)){
+
+            return 0;
+        }
+        $number = (int) $number;
 
         return $number > 0 ? -1 * $number : abs($number);
     }
